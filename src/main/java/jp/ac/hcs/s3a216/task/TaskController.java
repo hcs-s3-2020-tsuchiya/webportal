853d3aa0bc19e6f7e108cc20875144f8111750f6
s20192004 <s20192004@HCS19058.stdlan.hcs.local>
2021-07-05 09:42:10 +0900

    一覧

--- src/main/java/jp/ac/hcs/s3a216/task/TaskController.java
+++ src/main/java/jp/ac/hcs/s3a216/task/TaskController.java
@@ -7,17 +7,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Controller
 public class TaskController {
 
 	@Autowired
 	private TaskService taskService;
 	
-	@PostMapping("/task")
-	public String getTask(Principal principal,Model model) {
+/*
+	@PostMapping("/task/insert")
+	public String insertTask(@RequestParam("comment") String comment,@RequestParam("limitday")String limitday,
+			Principal principal,Model model) {
+		
+		log.info("[" + principal.getName()+"]タスク追加:"+ "コメント"+ comment + "期限日:" + limitday);
+		boolean isSuccess = taskService.insert(principal.getName(),comment,limitday);
+		
+		if(isSuccess) {
+			log.info("成功");
+		}else {
+			log.info("失敗");
+		}
+		
+		return getTaskList(principal,model);
+	}
+	*/
+	
+
+	
+	@PostMapping("/task/tasklist")
+	public String getTaskList(Principal principal,Model model) {
+		
+		log.info("[" + principal.getName()+"]タスク検索:" + principal.getName());
+		
+		
+		TaskEntity taskEntity = taskService.selectAll(principal.getName());
+		if(taskEntity == null) {
+			log.info("[" + principal.getName()+"]タスク検索:エラーが発生しました。");
+		}
+		model.addAttribute("taskEntity", taskEntity);
+		
 		
 		return "task/task";
 		
+		
+	
 	}
-
 }
+	
